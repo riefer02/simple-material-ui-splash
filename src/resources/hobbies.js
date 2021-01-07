ce50f2a8ffa60147d70d7fc0e93538d3ece71960
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2021-01-07 11:51:19 -0600

    initial commit

---
+++ src/resources/hobbies.js
@@ -0,0 +1,11 @@
+const hobbies = [
+  {
+    image:
+      "https://lh3.googleusercontent.com/proxy/Em9BN8Nt3yyS9BooW-VPAIbzo08ZNMrDVM4235zYT7kBfvg7pIGaivBfUz_i1ZLcCXreRkZbTeP3nNL4BbfsjRw9xXqrwVpnUVBuwy86YT0",
+    title: "Musician",
+    description: "I'm a rock star, band leader, and music producer!",
+    link: "https://www.thelewddudes.com/",
+  },
+];
+
+module.exports = hobbies;
---
+++ src/views/HomeView.js
@@ -0,0 +1,188 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Rief Productions
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [
+  {
+    image: "http://www.thelewddudes.com/img/whiteOakMusicHall-1.jpeg",
+    imageDescription: "andrew playing music",
+    title: "Musician",
+    description:
+      "I'm a rock star, the band leader, and music producer. Music is my passion. I enjoy working with other artists and co-writing songs together.",
+    link: "https://www.thelewddudes.com/",
+  },
+  {
+    image: "https://source.unsplash.com/random",
+    imageDescription: "andrew's developer portfolio",
+    title: "Developer",
+    description:
+      "I'm a self-taught full stack developer. I can create anything and everything. The only ingredients I need are patience and time.",
+    link: "https://www.riefer.io/",
+  },
+];
+
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Andrew Riefenstahl
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Andrew Riefenstahl
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              "The one note jam is more than just music, it's a consistent
+              thread that ties everything together. Then time steps in and says
+              I want to complicate things, but the one note jam stretches time,
+              revealing the jam."
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Visit Portfolio Site
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    onClick={() => console.log("hello")}
+                    variant="outlined"
+                    color="primary"
+                  >
+                    Visit Music Site
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card.title} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={card.image}
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.title}
+                    </Typography>
+                    <Typography>{card.description}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Go to website
+                    </Button>
+                    {/* <Button size="small" color="primary">
+                      Edit
+                    </Button> */}
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Full Stack Developer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Life's too short to drink cheap wine.
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
